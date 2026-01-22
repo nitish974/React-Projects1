a30ce784189ed5050c28e538d34cd5c8872bb5dd
nitish974 <nitish97411@gmail.com>
2026-01-22 16:42:53 +0530

    first commit

---
+++ 01-folder/src/assets/components/Card.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+        <div className='body'>
+            <div className='Card'>
+                <div className="top">
+                    <p>Shop by brand | Deals on top smartphones</p>
+                </div>
+                <div className="center">
+                    <img src="./images" alt="" />
+                </div>
+                <div className="bottom">
+                    <a href="#">see all others</a>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Card
